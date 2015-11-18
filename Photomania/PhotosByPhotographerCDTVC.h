a1fdd06cea8dedb8e6bb08a6d9d0cae163043f4f
Xinran <lindashi527@gmail.com>
2015-11-18 15:59:59 -0800

    add navigation, table view

---
+++ Photomania/PhotosByPhotographerCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  PhotosByPhotographerCDTVC.h
+//  Photomania
+//
+//  Created by Kaiqi on 11/18/15.
+//  Copyright © 2015 self.edu. All rights reserved.
+//
+
+#import "PhotosCDTVC.h"
+#import "Photographer.h"
+
+@interface PhotosByPhotographerCDTVC : PhotosCDTVC
+
+@property (nonatomic, strong) Photographer *photographer;
+@end
---
+++ Photomania/PhotosCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  PhotosCDTVC.h
+//  Photomania
+//
+//  Created by Kaiqi on 11/18/15.
+//  Copyright © 2015 self.edu. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface PhotosCDTVC : CoreDataTableViewController
+
+// hook up fetchedResultsController to any Photo fetch request
+// use @"Photo Cell" as your table view cell's reuse id
+@end
